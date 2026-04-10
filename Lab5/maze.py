c8580347e6e1e20a51316af50188e27dad6c541d
Vladi <506727700@qq.com>
2026-04-10 13:21:43 +0800

    完成Lab5的题目一

--- Lab5/maze.py
+++ Lab5/maze.py
@@ -28,10 +28,10 @@ def reset_visited():
     for j in range(width):
       visited[i][j] = False
 
-def dfs(x, y, path = []):
+def dfs(x, y, path=None):
+  if path is None:
+    path = []
   if not is_valid(x, y, visited):
-    return False
-  if(maze[x][y] == 1):
     return False
   visited[x][y] = True
 
@@ -63,6 +63,70 @@ def bfs():
         cur_state.append((pos[0]+dx, pos[1]+dy))
         que.append(((pos[0]+dx, pos[1]+dy), cur_state))
   return [] 
+
+def dls(x, y, path, limit = 40):
+  if(limit == 0):
+    return False
+  if not is_valid(x, y, visited):
+    return False
+  visited[x][y] = True
+
+  path.append((x, y))
+  if((x, y) == goal):
+    return True
+  for dx, dy in directions:
+    if(dls(x+dx, y+dy, path, limit-1)):
+      return True
+  path.pop()
+  
+  return False
+
+def iddfs(x, y, max_limit, path):
+  for limit in range(1, max_limit+1):
+    reset_visited()
+    path.clear()
+    if(dls(x, y, path, limit)):
+      print(f"IDDFS 找到路径，最大深度为{limit}")
+      return True
+  return False
+
+def dedfs():
+  v = [[0 for _ in range(width)] for _ in range(height)]
+  v[start[0]][start[1]] = 1
+  v[goal[0]][goal[1]] = 2
+  
+  que_1 = [(start, [start])]
+  que_2 = [(goal, [goal])]
+  
+  paths_1 = {start: [start]}
+  paths_2 = {goal: [goal]}
+
+  while que_1 and que_2:
+    pos_1, state_1 = que_1.pop(0)
+    for dx, dy in directions:
+      x, y = pos_1[0]+dx, pos_1[1]+dy
+      if 0 <= x < height and 0 <= y < width and maze[x][y] != 1:
+        if v[x][y] == 2:
+          return state_1 + paths_2[(x, y)][::-1][1:]
+        if v[x][y] == 0:
+          v[x][y] = 1
+          new_state = state_1 + [(x, y)]
+          paths_1[(x, y)] = new_state
+          que_1.append(((x, y), new_state))
+
+    pos_2, state_2 = que_2.pop(0)
+    for dx, dy in directions:
+      x, y = pos_2[0]+dx, pos_2[1]+dy
+      if 0 <= x < height and 0 <= y < width and maze[x][y] != 1:
+        if v[x][y] == 1:
+          return paths_1[(x, y)] + state_2[::-1][1:]
+        if v[x][y] == 0:
+          v[x][y] = 2
+          new_state = state_2 + [(x, y)]
+          paths_2[(x, y)] = new_state
+          que_2.append(((x, y), new_state))
+    
+  return []
 
 filename = input("请输入文件名")
 maze, start, goal = Maze(filename)
--- Lab5/test1.py
+++ Lab5/test1.py
@@ -31,3 +31,51 @@ if result_bfs:
     print(f"BFS 路径为：{result_bfs}")
 else:
     print("BFS 未找到路径")
+
+
+# --- 3. 执行 DLS ---
+print("\n--- 执行 DLS ---")
+path = []
+for i in range(height):
+    for j in range(width):
+        visited[i][j] = False
+if dls(start[0], start[1], path, limit=40):
+    res = maze.copy()
+    for (x, y) in path[1:-1]:
+        res[x][y] = 7
+    for x in res:
+        print(x)
+    print(f"DLS 路径为：{path}")
+else:
+    print("DLS 未找到路径")
+
+# --- 4. 执行 IDDFS ---
+print("\n--- 执行 IDDFS ---")
+path = []
+max_limit = 40
+for i in range(height):
+    for j in range(width):
+        visited[i][j] = False
+if iddfs(start[0], start[1], max_limit, path=path):
+    res = maze.copy()
+    for (x, y) in path[1:-1]:
+        res[x][y] = 7
+    for x in res:
+        print(x)
+    print(f"IDDFS 路径为：{path}")
+else:
+    print(f"IDDFS 未找到路径")
+
+# --- 5. 执行 DDFS ---
+print("\n--- 执行 DDFS ---")
+result_dedfs = dedfs()
+
+if result_dedfs:
+    res = maze.copy()
+    for (x, y) in result_dedfs[1:-1]:
+        res[x][y] = 7
+    for x in res:
+        print(x)
+    print(f"DDFS 路径为：{result_dedfs}")
+else:
+    print("DDFS 未找到路径")
