4dceffc9ef5af361ef9ee31a4ad18b8989d0499a
Vladi <506727700@qq.com>
2026-04-16 11:17:37 +0800

    优化了细节，但是复杂度依然高

--- Lab5/puzzle.py
+++ Lab5/puzzle.py
@@ -6,7 +6,7 @@ def Puzzle(filename):
   with open(filename, 'r') as f:
     lines = f.readlines()
     for line in lines:
-      puzzle.append([int(x) for x in line.strip()])
+      puzzle.append([int(x) for x in line.split(" ")])
     return puzzle
 
 def h(cur):
@@ -15,9 +15,10 @@ def h(cur):
   for i in range(len(cur)):
     for j in range(len(cur[i])):
       if(cur[i][j] != 0):
-        x, y = (cur[i][j])//len(cur[i]), (cur[i][j])%len(cur[i])
+        x, y = (cur[i][j]-1)//len(cur[i]), (cur[i][j]-1)%len(cur[i])
         distance += abs(x-i) + abs(y-j)
   return distance
+
 def get_zero(cur):
   """获取0的位置"""
   for i in range(len(cur)):
@@ -38,21 +39,23 @@ def neighbors(cur):
 
 def A_star(filename):
   puzzle = Puzzle(filename)
-  goal = [[i+j*len(puzzle) for i in range(len(puzzle))] for j in range(len(puzzle))]
+  goal = [[1+i+j*len(puzzle) for i in range(len(puzzle))] for j in range(len(puzzle))]
+  goal[-1][-1] = 0
 
   open = []
-  heapq.heappush(open, (h(puzzle), puzzle, 0))
-  close = []
-  close.append(puzzle)
+  heapq.heappush(open, (h(puzzle), 0, puzzle))
+  close = set()
+  close.add(tuple(tuple(row) for row in puzzle))
 
   while(len(open) > 0):
-    _, cur, g = heapq.heappop(open)
+    _, g, cur = heapq.heappop(open)
     if(cur == goal):
       return cur
     
     for neighbor in neighbors(cur):
-      if neighbor in close or neighbor in open:
+      neighbor_tuple = tuple(tuple(row) for row in neighbor)
+      if neighbor_tuple in close or neighbor_tuple in [x[2] for x in open]:
         continue
-      heapq.heappush(open, (h(neighbor), neighbor, g+1))
-      close.append(neighbor)
+      heapq.heappush(open, (h(neighbor)+g+1, g+1, neighbor))
+      close.add(neighbor_tuple)
   return None
--- Lab5/test2.py
+++ Lab5/test2.py
@@ -1,8 +1,17 @@
 import puzzle
 from puzzle import *
-import heapq
-from collections import deque
+
 
 """puzzle1"""
+print("--------------puzzle1:---------------")
 res1 = A_star('puzzle1.txt')
-print(res1)
+for x in res1:
+  print(x)
+print("-------------------------------------")
+
+"""puzzle2"""
+print("--------------puzzle2:---------------")
+res2 = A_star('puzzle2.txt')
+for x in res2:
+  print(x)
+print("-------------------------------------")
